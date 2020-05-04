6a614ab7511d006f7c3a927e73064951f30d0969
Adil Naimi <adilnm23@gmail.com>
2020-05-04 15:33:26 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
       resources :courses
     end
   end
-  
+  namespace :api do
+    namespace :v1 do
+      root to: 'courses#index'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
